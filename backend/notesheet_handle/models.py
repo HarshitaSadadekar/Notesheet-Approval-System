ed3841ea7b64d3989f3058038ffe9752fc794518
Aashish Sharma <74376468+the-hercules@users.noreply.github.com>
2023-03-13 22:02:41 +0530

    added backend files

---
+++ backend/notesheet_handle/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+# Create your models here
+class FacultyDetails(models.Model):
+    employee_id = models.CharField(max_length=20)
+    Name = models.CharField(max_length=50)
+
+
+class NoteSheet(models.Model):
+    f_id = models.ForeignKey(
+        FacultyDetails, on_delete=models.SET_NULL, null=True)
+    id = models.IntegerField(primary_key=True)
+    date = models.DateTimeField(auto_now_add=True)
+    school = models.CharField(max_length=100)
+    subject = models.TextField()
+    department = models.CharField(max_length=100)
+    description = models.TextField()
+    objective = models.TextField()
+    proposal_details = models.TextField()
+    proposal_submitted_by = models.CharField(max_length=255)
+    proposal_submitted_by_1 = models.CharField(max_length=255)
+    name_of_hod = models.CharField(max_length=255)
+    name_of_director = models.CharField(max_length=255)
+
+
+class NoteSheetAdditional(models.Model):
+    f_id = models.ForeignKey(NoteSheet, on_delete=models.SET_NULL, null=True)
+    Status = models.IntegerField()
