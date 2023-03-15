8c82807fe04f4c346a92fd627bf5fff4e6befc66
Kikii <harshitasadadekar@gmail.com>
2023-03-15 19:40:34 +0530

    added Handles for approve, reject, review, forward

--- src/Page/Dashboard/index.js
+++ src/Page/Dashboard/index.js
@@ -11,6 +11,100 @@ function Dashboard(){
 
     const [faculty, setFaculty] = useState(facultyData);
     const [isCreating, setIsCreating] = useState(false);
+    const [isApproving, setIsApproving] = useState(false);
+    const [isRejecting, setIsRejecting] = useState(false);
+    const [isReviewing, setIsReviewing] = useState(false);
+    const [isForwarding, setIsForwarding] = useState(false);
+
+    const handleApprove = (id) => {
+        Swal.fire({
+            icon: 'question',
+            title: 'Approve',
+            text: "Are you sure?",
+            showCancelButton: true,
+            confirmButtonText: 'Yes, Approve it!',
+            cancelButtonText: 'No, cancel!',
+        }).then(result => {
+            if(result.value){
+                // Enter Code
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Approved',
+                    // Enter Code for text
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+                // Enter Code
+            }
+        });
+    }
+    const handleReject = (id) => {
+        Swal.fire({
+            icon: 'question',
+            title: 'Reject',
+            text: "Are you sure?",
+            showCancelButton: true,
+            confirmButtonText: 'Yes, Reject it!',
+            cancelButtonText: 'No, cancel!',
+        }).then(result => {
+            if(result.value){
+                // Enter Code
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Rejected',
+                    // Enter Code for text
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+                // Enter Code
+            }
+        });
+    }
+    const handleReview = (id) => {
+        Swal.fire({
+            icon: 'question',
+            title: 'Review',
+            text: "Are you sure?",
+            showCancelButton: true,
+            confirmButtonText: 'Yes, Review it!',
+            cancelButtonText: 'No, cancel!',
+        }).then(result => {
+            if(result.value){
+                // Enter Code
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Reviewed',
+                    // Enter Code for text
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+                // Enter Code
+            }
+        });
+    }
+    const handleForward = (id) => {
+        Swal.fire({
+            icon: 'question',
+            title: 'Forward',
+            text: "Are you sure?",
+            showCancelButton: true,
+            confirmButtonText: 'Yes, Forward it!',
+            cancelButtonText: 'No, cancel!',
+        }).then(result => {
+            if(result.value){
+                // Enter Code
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Forwarded',
+                    // Enter Code for text
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+                // Enter Code
+            }
+        });
+    }
+
 
     return (
         <div className='container'>
